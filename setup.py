b490bb190e0f9d355d6da1929190874c8b804658
dushyantRathore <dushyant.bgs@gmail.com>
2017-04-03 18:51:42 +0530

    Updated

--- setup.py
+++ setup.py
@@ -24,13 +24,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
     ],
     keywords = "Weather notification tool",
     author_email='dushyant.bgs@gmail.com',
